db5e30beeb5f2db4c3d1c7cee8b2335a6a1b7019
FelixDauber <71184613+FelixDauber@users.noreply.github.com>
2022-01-29 11:48:02 +0100

    Renames some methods in the AI

--- Assets/Scripts/AI/CatAIWPathfinding.cs
+++ Assets/Scripts/AI/CatAIWPathfinding.cs
@@ -7,8 +7,6 @@ public class CatAIWPathfinding : MonoBehaviour
 {
     NavMeshAgent navMeshAgent;
     static GameObject player;
-    Rigidbody rigidbody;
-    Vector3 lastPosition;
 
     // Start is called before the first frame update
     void Awake()
@@ -23,28 +21,22 @@ public class CatAIWPathfinding : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        MoveToPlayer();
-        RotateWithVelocity();
+        MoveTowardsPlayer();
+        RotateToNextWaypoint();
     }
 
-    void RotateWithVelocity()
+    void RotateToNextWaypoint()
     {
         if (navMeshAgent.path == null || navMeshAgent.path.corners.Length < 2) return;
         var diff = navMeshAgent.path.corners[1] - transform.position;
         diff.Normalize();
         float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0f, 0f, rot_z - 90);
-        //Debug.Log(navMeshAgent.nextPosition);
-        
-        //transform.right = player.transform.position - transform.position;
-        //transform.up = new Vector3(player.transform.position.x, player.transform.position.y, 0) - transform.position;
-        //transform.rotation = Quaternion.AngleAxis(Vector3.Angle(transform.position, player.transform.position), Vector3.forward);
     }
 
-    void MoveToPlayer()
+    void MoveTowardsPlayer()
     {
         navMeshAgent.destination = player.transform.position;
-        lastPosition = transform.position;
     }
 
     private void OnDrawGizmos()
