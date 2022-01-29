7f70447e11032d157799e2c5b2da8557aa253be9
FelixDauber <71184613+FelixDauber@users.noreply.github.com>
2022-01-29 19:43:58 +0100

    Adds Cat animations

--- Assets/Scripts/Animator/SimplestAnimatorPossible.cs
+++ Assets/Scripts/Animator/SimplestAnimatorPossible.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class SimplestAnimatorPossible : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Sprite sprite1, sprite2;
+
+    public Sprite CurrentSprite => spriteRenderer.sprite;
+    public SpriteRenderer spriteRenderer;
+
+    public float timePerSprite = 0.3f;
+    float variation;
+
+    private void Start()
     {
-        
+        variation = Random.Range(0, timePerSprite * 2);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if(Mathf.PingPong(Time.time + variation, timePerSprite*2 + variation / 2) - timePerSprite > 0)
+        {
+            if(CurrentSprite != sprite1)
+            {
+                spriteRenderer.sprite = sprite1;
+            }
+        }
+        else
+        {
+            if (CurrentSprite != sprite2)
+            {
+                spriteRenderer.sprite = sprite2;
+            }
+        }
     }
 }
