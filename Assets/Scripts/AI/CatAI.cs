6311a913a42ed2ab1c50e7fa408c50047958f0c9
FelixDauber <71184613+FelixDauber@users.noreply.github.com>
2022-01-29 20:26:41 +0100

    Adds another animator, Makes cats purr

--- Assets/Scripts/AI/CatAI.cs
+++ Assets/Scripts/AI/CatAI.cs
@@ -30,6 +30,11 @@ public class CatAI : MonoBehaviour
 
     public void OnHit()
     {
-        Destroy(gameObject);
+        enabled = false;
+        GetComponent<SimplestAnimatorPossible>().enabled = false;
+        GetComponent<Collider2D>().enabled = false;
+        var almostSimplestAnimator = GetComponent<AlmostSimplestAnimator>();
+        almostSimplestAnimator.enabled = true;
+        almostSimplestAnimator.onCompleted.AddListener(delegate { Destroy(gameObject); });
     }
 }
